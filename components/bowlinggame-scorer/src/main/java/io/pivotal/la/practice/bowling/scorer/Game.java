e7c984aa10e920734a7a61fe6e91ba828368e57d
John S. Ryan <jtigger@infosysengr.com>
2017-09-07 20:00:53 -0700

    Frames now track number of bowls.

--- components/bowlinggame-scorer/src/main/java/io/pivotal/la/practice/bowling/scorer/Game.java
+++ components/bowlinggame-scorer/src/main/java/io/pivotal/la/practice/bowling/scorer/Game.java
@@ -7,15 +7,16 @@ public class Game {
 	private int score = 0;
 	private int pinsStanding = 10;
 	private int frame = 1;
-	private int bowlsInFrame = 0;
 	private Bonus bonus1 = new Bonus();
 	private Bonus bonus2 = new Bonus();
 	private int extraBowl = 0;
 	private List<Frame> frames;
+	private Frame currentFrame;
 
 	public Game() {
 		frames = new ArrayList<>();
-		frames.add(new Frame());
+		currentFrame = new Frame();
+		frames.add(currentFrame);
 	}
 
 	public int score() {
@@ -31,7 +32,7 @@ public class Game {
 		}
 		score += pinsKnockedDownThisBowl;
 		pinsStanding -= pinsKnockedDownThisBowl;
-		bowlsInFrame++;
+		currentFrame.bowls++;
 
 		if (bonus1.pendingBonusCount > 0) {
 			bonus1.bonus += pinsKnockedDownThisBowl;
@@ -49,7 +50,7 @@ public class Game {
 		}
 		if (frames.size() < 10) {
 			if (pinsStanding == 0) {
-				if (bowlsInFrame == 1) {
+				if (currentFrame.bowls == 1) {
 					if (bonus1.pendingBonusCount == 0) {
 						bonus1.bonus = 0;
 						bonus1.pendingBonusCount = 2;
@@ -57,9 +58,9 @@ public class Game {
 						bonus2.bonus = 0;
 						bonus2.pendingBonusCount = 2;
 					}
-					frames.add(new Frame());
+					currentFrame = new Frame();
+					frames.add(currentFrame);
 					pinsStanding = 10;
-					bowlsInFrame = 0;
 				} else {
 					if (bonus1.pendingBonusCount == 0) {
 						bonus1.bonus = 0;
@@ -68,25 +69,25 @@ public class Game {
 						bonus2.bonus = 0;
 						bonus2.pendingBonusCount = 1;
 					}
-					frames.add(new Frame());
+					currentFrame = new Frame();
+					frames.add(currentFrame);
 					pinsStanding = 10;
-					bowlsInFrame = 0;
 				}
 			}
-			if (bowlsInFrame == 2) {
-				frames.add(new Frame());
+			if (currentFrame.bowls == 2) {
+				currentFrame = new Frame();
+				frames.add(currentFrame);
 				pinsStanding = 10;
-				bowlsInFrame = 0;
 			}
 		} else {
 			if (pinsStanding == 0) {
 				pinsStanding = 10;
 				extraBowl = 1;
 			}
-			if (bowlsInFrame == (2 + extraBowl)) {
-				frames.add(new Frame());
+			if (currentFrame.bowls == (2 + extraBowl)) {
+				currentFrame = new Frame();
+				frames.add(currentFrame);
 				pinsStanding = 10;
-				bowlsInFrame = 0;
 			}
 		}
 	}
@@ -101,6 +102,7 @@ public class Game {
 }
 
 class Frame {
+	int bowls = 0;
 }
 
 class Bonus {
